8c4bf697a09001b725602c00556ffd58c80e8b24
Zephyr-Jeon <rltjd1258@gmail.com>
2024-02-26 22:09:43 -0800

    first commit

---
+++ src/elasticsearch.ts
@@ -0,0 +1,38 @@
+import { Client } from '@elastic/elasticsearch';
+import { ClusterHealthResponse } from '@elastic/elasticsearch/lib/api/types';
+import { config } from '@gateway/config';
+import { winstonLogger } from '@Zephyr-Jeon/jobber-shared';
+import { Logger } from 'winston';
+
+const log: Logger = winstonLogger(`${config.ELASTIC_SEARCH_URL}`, 'apiGatewayElasticConnection', 'debug');
+
+class ElasticSearch {
+  private elasticSearchClient: Client;
+
+  constructor() {
+    this.elasticSearchClient = new Client({
+      node: `${config.ELASTIC_SEARCH_URL}`
+    });
+  }
+
+  public async checkConnection(): Promise<void> {
+    let isConnected = false;
+    let count = 0;
+
+    while (!isConnected || count > 1) {
+      log.info('GatewayService Connecting to ElasticSearch');
+      count++;
+
+      try {
+        const health: ClusterHealthResponse = await this.elasticSearchClient.cluster.health({});
+        log.info(`GatewayService ElasticSearch health status - ${health.status}`);
+        isConnected = true;
+      } catch (error) {
+        log.error('Connection to ElasticSearch failed, Retrying...');
+        log.log('error', 'GatewayService checkConnection() method error:', error);
+      }
+    }
+  }
+}
+
+export const elasticSearch: ElasticSearch = new ElasticSearch();
