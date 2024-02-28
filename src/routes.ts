8549b70a6b168024b8c163c92dc44d251013427e
Zephyr-Jeon <rltjd1258@gmail.com>
2024-02-27 21:03:03 -0800

    added controller and routes for auth service

--- src/routes.ts
+++ src/routes.ts
@@ -1,8 +1,8 @@
 import { Application } from 'express';
 import { healthRoutes } from '@gateway/routes/health';
-// import { authRoutes } from '@gateway/routes/auth';
-// import { currentUserRoutes } from '@gateway/routes/current-user';
-// import { authMiddleware } from '@gateway/services/auth-middleware';
+import { authRoutes } from '@gateway/routes/auth';
+import { currentUserRoutes } from '@gateway/routes/current-user';
+import { authMiddleware } from '@gateway/services/auth-middleware';
 // import { searchRoutes } from '@gateway/routes/search';
 // import { buyerRoutes } from '@gateway/routes/buyer';
 // import { sellerRoutes } from '@gateway/routes/seller';
@@ -15,9 +15,9 @@ const BASE_PATH = '/api/gateway/v1';
 
 export const appRoutes = (app: Application) => {
   app.use('', healthRoutes.routes());
-  // app.use(BASE_PATH, authRoutes.routes());
+  app.use(BASE_PATH, authRoutes.routes());
   // app.use(BASE_PATH, searchRoutes.routes());
-  // app.use(BASE_PATH, authMiddleware.verifyUser, currentUserRoutes.routes());
+  app.use(BASE_PATH, authMiddleware.verifyUser, currentUserRoutes.routes());
   // app.use(BASE_PATH, authMiddleware.verifyUser, buyerRoutes.routes());
   // app.use(BASE_PATH, authMiddleware.verifyUser, sellerRoutes.routes());
   // app.use(BASE_PATH, authMiddleware.verifyUser, gigRoutes.routes());
